1488191f1bc2b7cfc1f591eeeb8d3862e82c9731
R3iZer0 <94191193+R3iZer0@users.noreply.github.com>
2022-05-15 21:26:08 +0200

    Add files via upload

---
+++ possibleUsername.py
@@ -0,0 +1,20 @@
+print("-----------------------------------------------------------------------------")
+print("                Possible Social Media Username for Sherlock \n                     **dont use capital letters")
+print("-----------------------------------------------------------------------------")
+
+name = input("Enter name:")
+lastname=input("Enter lastname:")
+print(name+"."+lastname , 
+name+"-"+lastname ,
+name+"_"+lastname, 
+lastname+"."+name, 
+lastname+"-"+name, 
+lastname+"_"+name,
+name.capitalize()+"."+lastname ,
+name.capitalize()+"-"+lastname ,
+name.capitalize()+"_"+lastname, 
+lastname+"."+name.capitalize(), 
+lastname+"-"+name.capitalize(),
+lastname+"_"+name.capitalize()
+
+)
